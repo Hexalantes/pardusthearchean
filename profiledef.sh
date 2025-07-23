6facef7204ab654b5a11882d732cf9ccda835191
Hexalantes <134841282+Hexalantes@users.noreply.github.com>
2025-07-23 17:26:22 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -12,8 +12,6 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
            'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
-kernel='linux-zen'
-mkinitcpio_preset='linux-zen.preset'
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
